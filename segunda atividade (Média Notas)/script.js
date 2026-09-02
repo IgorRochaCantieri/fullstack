aab8ae9406ebc9df52047c8eae3021cee9f6b7da
Igor Rocha Cantieri <igorrocha2198@gmail.com>
2026-09-01 22:35:09 -0300

    l

--- segunda atividade (Média Notas)/script.js
+++ segunda atividade (Média Notas)/script.js
@@ -1,44 +1,45 @@
 
 let notas = document.querySelectorAll('.notas');
-
-
+let notaMedia = 0;
+let legendas = document.querySelectorAll('.legendas');
 
 function enviaForm(){
     //Depois vamos verificar se algum campo de nota está vazio
-    
     let nome = document.getElementById('nome').value;
-    let quatroNotas = 0;
-    let notaMedia = 0;
-
-    let legendas = document.querySelectorAll('.legendas');
-
     if(nome === ""){alert("Digite o nome do aluno!")}
+    notaMedia = calcMedia();
+    document.getElementById('mediaReal').innerHTML = notaMedia;
+}
 
+function calcMedia(){
+    let notaDosAlunos = 0;
     for(var i=0; i<notas.length; i++){
         if(notas[i].value === ""){
             alert(`O campo da ${legendas[i].innerHTML} está vazio!`);
+            return;
         }else{
-            quatroNotas += Number(notas[i].value);
+            notaDosAlunos += Number(notas[i].value);
         }
     }
-
-    notaMedia = quatroNotas/notas.length;
-
-    document.getElementById('mediaReal').innerHTML = notaMedia;
-
+    notaMedia = notaDosAlunos/notas.length;
+    return notaMedia;
 }
 
 function calcMediaAuto(){
     for(var i=0; i<notas.length; i++){
         notas[i].addEventListener("input", function(){
-            console.log("Nota alterada");
+            let notaMedia = calcMedia();
+            document.getElementById('mediaReal').innerHTML = notaMedia;
         });
     }
 }
 
+
+/*
 document.addEventListener("DOMContentLoaded", function(){
     calcMediaAuto();
 })
+    */
 
 
 
